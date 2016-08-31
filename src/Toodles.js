65e20f4ac28095822a17dac0a3753fd684edec4b
clj13001 <connor.jackson@uconn.edu>
2016-08-30 22:30:51 -0400

    Allows for more than one type of list!
    Much more generalization. Now more lists can be supported!! :)

--- src/Toodles.js
+++ src/Toodles.js
@@ -3,18 +3,37 @@ var backgrounds = ['tamra.gif','nene.gif','tati.gif','kylie.gif','bicth.gif','em
 $('body').css({'background-image': 'url(images/backgrounds/' + backgrounds[Math.floor(Math.random() * backgrounds.length)] + ')'});
 
 //shows all the to-dos
-show();
+show('todo');
+show('wishlist');
 
 //automatically adds listeners to the create button and for entering in the text field
-document.getElementById('add').addEventListener('click', function(){add(document.getElementById('task').value)});
-document.getElementById('task').addEventListener('keypress',function(e){if(e.keyCode===13){add(this.value);}});
+var buttons = document.getElementsByClassName('add');
+for(var a=0; a<buttons.length; a++){
+	var buttontype = buttons[a].getAttribute('data-type');
+	if(buttontype.localeCompare('todo')==0){
+		buttons[a].addEventListener('click', function(){add(document.getElementById('todo').value,'todo')});
+	}
+	else{
+		buttons[a].addEventListener('click', function(){add(document.getElementById('wishlist').value,'wishlist')});
+	}
+}
+var inputs = document.getElementsByClassName('task');
+for(var b=0; b<inputs.length; b++){
+	var inputtype = inputs[b].getAttribute('data-type');
+	if(inputtype.localeCompare('todo')==0){
+		inputs[b].addEventListener('keypress', function(e){if(e.keyCode===13){add(document.getElementById('todo').value,'todo')}});
+	}
+	else{
+		inputs[b].addEventListener('keypress', function(e){if(e.keyCode===13){add(document.getElementById('wishlist').value,'wishlist')}});
+	}
+}
 
 //get all the to-do objects.
-function get_todos() {
+function get_todos(type) {
 	//initiate array for them
     var todos = new Array;
 	//get them from local storage
-    var todos_str = localStorage.getItem('todo');
+    var todos_str = localStorage.getItem(type);
 	//if there is at least one object already we need to convert it from JSON to string
     if (todos_str !== null) {
         todos = JSON.parse(todos_str); 
@@ -23,53 +42,53 @@ function get_todos() {
 }
  
  //add a task
-function add(task) {
+function add(task, type) {
 	//add date to end of task
 	task = task + ' - ' + getCurrentDate();
 	//get all the other todos
-    var todos = get_todos();
+    var todos = get_todos(type);
 	//put this one at the bottom
     todos.push(task);
 	//add the string to local storage with 'todo' tags
-    localStorage.setItem('todo', JSON.stringify(todos));
+    localStorage.setItem(type, JSON.stringify(todos));
 	
 	
-    show();
+    show(type);
 	
 	//empty input field
-	document.getElementById('task').value = '';
+	document.getElementById(type).value = '';
  
     return false;
 }
 
 //remove a task
-function remove(id,timeout) {
+function remove(id,timeout,type) {
 	//get all other todos
-    var todos = get_todos();
+    var todos = get_todos(type);
 	//delay
 	setTimeout(function(){
 		//take out that todo
 		todos.splice(id, 1);
 		//re-add all the todos to the local storage
-		localStorage.setItem('todo', JSON.stringify(todos));
+		localStorage.setItem(type, JSON.stringify(todos));
 	
 		//show them all now
-		show();
+		show(type);
 		return false;
 	},(timeout));
 }
  
  //show all tasks
-function show() {
+function show(type) {
 	//get all todos
-    var todos = get_todos();
+    var todos = get_todos(type);
 	
 	//begin unordered list
     var html = '<ul>';
 	//iterate through all todos and put them into list tags, with in-order numerical IDs
     if(todos.length !== 0){
 		for(var i=0; i<todos.length; i++) {
-			html += '<li>' + todos[i] + '	' + '<img class ="edit" src="images/edit_icon.png" id="' + i + '"></img><input class="remove" type = "checkbox" id="' + i  + '"></input></li>';
+			html += '<li>' + todos[i] + '	' + '<img class ="edit" src="images/edit_icon.png" id="' + i + '" data-type = "' + type + '"></img><input class="remove" type = "checkbox" id="' + i  + '"></input></li>';
 		};
 	}
 	else{
@@ -79,16 +98,17 @@ function show() {
     html += '</ul>';
  
 	//change the inner HTML to be what you just got
-    document.getElementById('todos').innerHTML = html;
+	var div = type + 'div';
+    document.getElementById(div).innerHTML = html;
  
 	//for each todo you just placed, give the checkboxes event listeners
     var checkboxes = document.getElementsByClassName('remove');
     for (var i=0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('click', function(){remove(this.getAttribute('id'),1000);});
+        checkboxes[i].addEventListener('click', function(){remove(this.getAttribute('id'),1000,type);});
     };
 	
 	//set up edit buttons with event listeners
-	var editbuttons = document.getElementsByClassName('edit')
+	var editbuttons = document.getElementsByClassName('edit');
 	for(var i = 0; i < editbuttons.length; i++){
 		editbuttons[i].addEventListener('click',edit);
 	}
@@ -121,8 +141,9 @@ function getCurrentDate(){
 function edit(){
 	//get the edit ID
 	var id = this.getAttribute('id');
+	var type = this.getAttribute('data-type');
 	
-	var todos = get_todos();
+	var todos = get_todos(type);
 	//get the todo we are looking for, but we have to take off the date for the autofill...
 	var task = todos[id];
 	var prefill = '';
@@ -140,10 +161,8 @@ function edit(){
 	}
 	
 	else{
-	add(change);
-	remove(id,0);
+	add(change, type);
+	remove(id,0, type);
 	return false;
 	}	
 }
-
- 
